3ea2ec798c584299ff990cb2c345f73f6c007078
JordiRDL <hariran32@gmail.com>
2026-01-27 12:55:23 +0200

    mise à jour app mobile
    -> sidebar à jour

---
+++ mobile-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.antananarivo.signalement',
+  appName: 'Signalement Routier',
+  webDir: 'dist'
+};
+
+export default config;
